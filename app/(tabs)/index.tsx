87d27bd80738c32a3f87fb4c4c2b41269d03ab4d
zaynmuhammad <zaynmuhammad95@gmail.com>
2025-10-17 23:46:21 -0400

    Setting up nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,11 @@
-import { StyleSheet, View } from "react-native";
-
-import { ThemedText } from "@/components/themed-text";
-
-
+import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <ThemedText>Hello</ThemedText>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
